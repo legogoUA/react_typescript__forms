4f17611d3b7d987f86736bccb877dce483718431
legogoUA <olehmedushivskyi@gmail.com>
2024-02-23 14:58:38 +0200

    add basic structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react_typesctipt__forms
 Forms with React + TypeScript
+
+> creating basics structure 
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import { App } from './App';
+
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+
+root.render(
+  <App />
+)
